9b21dadc1845624f76090b1a7e0fc98ea7fdedf1
wuwx <wuweixin@gmail.com>
2020-03-05 23:52:02 +0800

    support http

--- src/NeuqidsProvider.php
+++ src/NeuqidsProvider.php
@@ -1,17 +1,34 @@
 <?php
 namespace Wuwx\LaravelSocialiteNeuqids;
 
-class NeuqidsProvider {
+use Closure;
+use Illuminate\Support\Facades\Session;
+use Laravel\Socialite\Contracts\Provider as ProviderContract;
+use phpCAS;
 
+class NeuqidsProvider implements ProviderContract
+{
     public function __construct()
     {
+        session_set_save_handler(Session::getHandler());
+
         phpCAS::client(CAS_VERSION_2_0, "ids.neuq.edu.cn", 443, "authserver");
+
+        Closure::bind(static function() {
+            return self::$_PHPCAS_CLIENT->setBaseURL("http://ids.neuq.edu.cn/authserver/");
+        }, null, phpCAS::class)();
+
         phpCAS::setNoCasServerValidation();
+    }
+
+    public function redirectUrl($url)
+    {
+        phpCAS::setFixedServiceURL($url);
+        return $this;
     }
 
     public function redirect()
     {
-        phpCAS::setFixedServiceURL(phpCAS::getServiceURL() . "/callback");
         return redirect(phpCAS::getServerLoginURL());
     }
 
